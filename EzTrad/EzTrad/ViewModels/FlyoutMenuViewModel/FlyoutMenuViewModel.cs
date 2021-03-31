cb87a657f2e453e5e63cab7ddeabd0c827ab7b3e
thiennguyen1007 <thienhoasac@gmail.com>
2021-03-31 16:09:25 +0700

    ""

--- EzTrad/EzTrad/ViewModels/FlyoutMenuViewModel/FlyoutMenuViewModel.cs
+++ EzTrad/EzTrad/ViewModels/FlyoutMenuViewModel/FlyoutMenuViewModel.cs
@@ -1,10 +1,81 @@
 ﻿using EzTrad.Models;
+using EzTrad.Services;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace EzTrad.ViewModels.FlyoutMenuViewModel
 {
-    public class FlyoutMenuViewModel
+    public class FlyoutMenuViewModel : BaseViewModel
     {
+        private IPageService _pageService;
+        private ObservableCollection<Flyout> _flyoutItems;
+        public string StarColor 
+        { 
+            get => _starColor; 
+            set => SetProperty(ref _starColor, value); 
+        }
+        private bool _starTick;
+        private string _starColor = "Transparent";
+
+        //Command
+        public ICommand LoadDataCommand { get; set; }
+        public ICommand LogoutCommand { get; set; }
+        public ICommand SettingCommand { get; set; }
+        public ICommand FavoritesCommand { get; set; }
+        //
+        public ObservableCollection<Flyout> FlyoutItems
+        {
+            get => _flyoutItems;
+            set { SetProperty(ref _flyoutItems, value); }
+        }
+        public bool StarTick
+        {
+            get => _starTick;
+            set { SetProperty(ref _starTick, value); }
+        }
+        public FlyoutMenuViewModel(IPageService pageService)
+        {
+            _pageService = pageService;
+            //Command
+            LoadDataCommand = new Command(LoadData);
+            LogoutCommand = new Command(OnLogoutClicked);
+            FavoritesCommand = new Command(OnFavoritesClicked);
+            SettingCommand = new Command(OnSettingClicked);
+        }
+        private void LoadData()
+        {
+            FlyoutItems = new ObservableCollection<Flyout>(LstMenu());
+            StarTick = IsBusy;
+        }
+        private void OnLogoutClicked()
+        {
+            _pageService.DisplayAlert("Alert!", "Log out now?", "Yes", "No");
+        }
+        private void OnSettingClicked()
+        {
+            if (IsBusy == false)
+            {
+                IsBusy = true;
+                StarTick = IsBusy;
+            }
+            else
+            {
+                IsBusy = false;
+                StarTick = IsBusy;
+            }
+        }
+        private void OnFavoritesClicked()
+        {
+            if (StarColor == "White")
+            {
+                StarColor = "#FFEB3B";
+            }
+            else
+            {
+                StarColor = "White";
+            }          
+        }
         private ObservableCollection<Flyout> LstMenu()
         {
             ObservableCollection<Flyout> lstTemp = new ObservableCollection<Flyout>();
@@ -13,7 +84,55 @@ namespace EzTrad.ViewModels.FlyoutMenuViewModel
                 Title = "Thị trường",
                 IsHeader = true
             };
+            Flyout f1 = new Flyout
+            {
+                Title = "Tổng quan",
+                Icon = "tongQuan.png",
+            };
+            Flyout f2 = new Flyout
+            {
+                Title = "Bảng giá",
+                Icon = "BangGia.png",
+            };
+            Flyout f3 = new Flyout
+            {
+                Title = "Tin tức",
+                Icon = "news.png",
+            };
+            Flyout f4 = new Flyout
+            {
+                Title = "Chỉ số thế giới",
+                Icon = "index.png",
+            };
+            Flyout f5 = new Flyout
+            {
+                Title = "FPTS nhận định",
+                Icon = "nhanDinh.png",
+            };
+            Flyout f6 = new Flyout
+            {
+                Title = "Lịch sự kiện",
+                Icon = "calendar.png",
+            };
+            Flyout f7 = new Flyout
+            {
+                Title = "Biểu đồ",
+                Icon = "chart.png",
+            };
+            Flyout f8 = new Flyout
+            {
+                Title = "Giao dịch phái sinh",
+                Icon = "transaction.png",
+            };
             lstTemp.Add(f);
+            lstTemp.Add(f1);
+            lstTemp.Add(f2);
+            lstTemp.Add(f3);
+            lstTemp.Add(f4);
+            lstTemp.Add(f5);
+            lstTemp.Add(f6);
+            lstTemp.Add(f7);
+            lstTemp.Add(f8);
             return lstTemp;
         }
     }
--- EzTrad/EzTrad/Views/FlyoutMenuPage.xaml.cs
+++ EzTrad/EzTrad/Views/FlyoutMenuPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EzTrad.Services;
+using EzTrad.ViewModels.FlyoutMenuViewModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,49 +9,27 @@ namespace EzTrad.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FlyoutMenuPage : ContentPage
     {
-        public ObservableCollection<Models.Flyout> FlyoutItems { get; set; }
         public FlyoutMenuPage()
         {
+            var pageService = new PageService();
+            ViewModel = new FlyoutMenuViewModel(pageService);
+            this.BindingContext = ViewModel;
             InitializeComponent();
-            FlyoutItems = new ObservableCollection<Models.Flyout>()
-            {
-                new Models.Flyout{ Title="Thị trường",
-                    IsHeader=true
-                },
-                new Models.Flyout{ Title="Tổng quan",
-                    //TargetPage=typeof(Views.TongQuanPage),
-                    Icon="tongQuan.png",
-                },
-                new Models.Flyout{ Title="Bảng giá",
-                    //TargetPage=typeof(Views.About),
-                    Icon="BangGia.png"
-                },
-                new Models.Flyout{ Title="Tin tức",
-                    //TargetPage=typeof(Views.About),
-                    Icon="news.png"
-                },
-                new Models.Flyout{ Title="Chỉ số thế giới",
-                    //TargetPage=typeof(Views.About),
-                    Icon="index.png"
-                },
-                new Models.Flyout{ Title="FPTS nhận định",
-                    //TargetPage=typeof(Views.About),
-                    Icon="nhanDinh.png"
-                },
-                new Models.Flyout{ Title="Lịch sự kiện",
-                    //TargetPage=typeof(Views.About),
-                    Icon="calendar.png"
-                },
-                new Models.Flyout{ Title="Biểu đồ",
-                    //TargetPage=typeof(Views.About),
-                    Icon="chart.png"
-                },
-                new Models.Flyout{ Title="Giao dịch phái sinh",
-                    //TargetPage=typeof(Views.About),
-                    Icon="transaction.png"
-                },
-            };
-            listView.ItemsSource = FlyoutItems;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel.LoadDataCommand.Execute(null);
+        }
+        private FlyoutMenuViewModel ViewModel
+        {
+            get => BindingContext as FlyoutMenuViewModel;
+            set { BindingContext = value; }
+        }
+
+        private void ImageButton_Clicked(object sender, System.EventArgs e)
+        {
+            ViewModel.FavoritesCommand.Execute(null);
         }
     }
 }
